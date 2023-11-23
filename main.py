f0f1469c566ac5a4316801ac3020a1ef183fb0d6
Lungisani Ngcobo <mapholoba066@gmail.com>
2023-11-23 09:56:21 +0200

    add player spaceship and controls

--- main.py
+++ main.py
@@ -7,13 +7,28 @@ screen = pygame.display.set_mode((800, 600))
 
 #Title and icon
 pygame.display.set_caption("Space Invaders")
-icon = pygame.image.load('ufo.png')
+icon = pygame.image.load('icon.png')
 pygame.display.set_icon(icon)
+
+#Player
+playerimg = pygame.image.load('spaceship.png')
+playerX = 370
+playerY = 480
+
+def player(x,y):
+    screen.blit(playerimg, (x,y))
 
 #Game Loop
 running = True
 while running:
+
+    #RGB
+    screen.fill((0, 128, 128))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    player(playerX, playerY)
+    pygame.display.update()
 
